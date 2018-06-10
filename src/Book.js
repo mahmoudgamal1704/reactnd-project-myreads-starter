c76602a018b8aba31209ca74b0dae985a6e456f0
Mahmoud Gamal <mahmoudgamal1704@gmail.com>
2018-06-11 01:22:51 +0200

    Edit: Search page to display book without image

--- src/Book.js
+++ src/Book.js
@@ -1,15 +1,17 @@
 import React from 'react'
 
 class Book extends React.Component {
+
 	render(){
 		return(
 			<div className="bookshelf-books">
 				<ol className="books-grid">
 					{this.props.filtredbooks.length > 0 && 
-					this.props.filtredbooks.map(book => (<li key={book.id}> 
+					this.props.filtredbooks.map((book,index) => (
+					<li key={book.id}> 
 						<div className="book">
 							<div className="book-top">
-								<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+							<div className="book-cover" style={{ width: 128, height: 193,  backgroundImage: `url(${ book.imageLinks ? book.imageLinks.thumbnail : ""})`} }></div>
 								<div className="book-shelf-changer">
 									<select name='shelf' value = {book.shelf} onChange={e => this.props.change(e,book)}>
 										<option value="move" disabled>Move to...</option>
@@ -21,7 +23,7 @@ class Book extends React.Component {
                                 </div>
                             </div>
                             <div className="book-title">{book.title}</div>
-         					<div className="book-authors">{book.authors}</div>
+         					<div className="book-authors">{book.authors ? book.authors : " "}</div>
      					</div>
 					</li>))}
 				</ol>
